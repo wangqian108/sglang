43995b56feb1c84060507d8d77e53183c45ecb20
wangqian108 <wangqian108@huawei.com>
2025-06-18 12:49:28 +0800

    Update layernorm.py

--- python/sglang/srt/layers/layernorm.py
+++ python/sglang/srt/layers/layernorm.py
@@ -41,6 +41,9 @@ if _use_aiter:
 elif _is_hip:
     from vllm._custom_ops import fused_add_rms_norm, rms_norm
 
+if _is_npu:
+    import torch_npu
+
 logger = logging.getLogger(__name__)
 
 
@@ -121,6 +124,16 @@ class RMSNorm(CustomOp):
             return x
         else:
             return x, residual
+
+    def forward_npu(
+            self,
+            x: torch.Tensor,
+            residual: Optional[torch.Tensor] = None,
+    ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+        if residual is not None:
+            out, _, residual_out = torch_npu.npu_add_rms_norm(residual, x, self.weight.data, self.variance_epsilon)
+            return out, residual_out
+        return torch_npu.npu_rms_norm(x, self.weight.data, self.variance_epsilon)[0]
 
 
 class GemmaRMSNorm(CustomOp):
